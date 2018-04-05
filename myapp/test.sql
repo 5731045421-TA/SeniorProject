63127543f3f8ccf9bf302b77b31e9c78d02468aa
5731045421-TA <thanaphata79@gmail.com>
2018-04-06 00:56:49 +0700

    add id

---
+++ myapp/test.sql
@@ -0,0 +1,52 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.3
+-- Dumped by pg_dump version 10.3
+
+-- Started on 2018-04-05 21:21:37
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 8 (class 2615 OID 16496)
+-- Name: Test; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA "Test";
+
+
+ALTER SCHEMA "Test" OWNER TO postgres;
+
+--
+-- TOC entry 1 (class 3079 OID 12924)
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- TOC entry 2793 (class 0 OID 0)
+-- Dependencies: 1
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+-- Completed on 2018-04-05 21:21:37
+
+--
+-- PostgreSQL database dump complete
+--
+
